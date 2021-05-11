3546b61209cf83023efd1fe4276d9c52da3f9646
AlejandroSotoCastro <alejandrosc.eng@gmail.com>
2021-05-11 10:55:22 +0200

    details of the patients

--- src/pages/PatientDetails.js
+++ src/pages/PatientDetails.js
@@ -5,42 +5,51 @@ import axios from "axios";
 
 export default function PatientDetails() {
   const route_parameters = useParams();
-  const [fetechedData, set_fetechedData] = useState();
+
   const [patientData, set_patientData] = useState();
-
-  function findPatient() {
-    const patient = [...fetechedData].find((patient) => {
-      console.log(patient.id === route_parameters.patientId);
-      return patient.id === route_parameters.patientId;
-    });
-
-    console.log("this is the patient", patient);
-
-    set_patientData(patient);
-
-    //clean buffer
-    set_fetechedData("");
-  }
 
   useEffect(() => {
     async function fetchPatients() {
       const response = await axios.get(
-        `https://my-json-server.typicode.com/Codaisseur/patient-doctor-data/patients`
+        `https://my-json-server.typicode.com/Codaisseur/patient-doctor-data/patients/${route_parameters.patientId}`
       );
 
-      set_fetechedData(response.data);
+      set_patientData(response.data);
     }
 
     fetchPatients();
   }, []);
 
-  if (fetechedData) {
-    findPatient();
-    console.log(patientData);
+  if (patientData) {
+    // findPatient();
+    console.log("patient data ", patientData);
 
     return (
       <div>
-        <h1>{(patientData.firstName, patientData.lastName)}</h1>
+        <div className="Border">
+          <h1>
+            {patientData.firstName} {patientData.lastName}
+          </h1>
+          <p>Date of birth {patientData.dateOfBirth}</p>
+          <p>Gender {patientData.gender}</p>
+        </div>
+
+        <div className="Border">
+          <p>Contact details</p>
+          <ul>
+            <li>email: {patientData.email}</li>
+            <li>phoneNumber: {patientData.phoneNumber}</li>
+          </ul>
+        </div>
+
+        <div className="Border">
+          <p>prescriptions</p>
+          <ul>
+            {patientData.prescriptions.map((prescription) => (
+              <li>{prescription}</li>
+            ))}
+          </ul>
+        </div>
       </div>
     );
   } else {
